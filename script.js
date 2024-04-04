409733d967245feeccee7493d5092a09fe8d7819
Lupis_ <doliver575@gmail.com>
2024-04-04 00:19:37 -0400

    create colored cards for books, create dialog window to add new books

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Library of Book Objects
 const myLibrary = [
     new Book("tempBook1", "tempAuthor1", 100, "read"),
-    new Book("tempBook2", "tempAuthor2", 200, "read"),
-    new Book("tempBook3", "tempAuthor3", 300, "not read"),
-    new Book("tempBook4", "tempAuthor4", 400, "not read"),
+    new Book("tempBook2", "tempAuthor2", 200, "reading"),
+    new Book("tempBook3", "tempAuthor3", 300, "unread"),
+    new Book("tempBook4", "tempAuthor4", 400, "read"),
+    new Book("tempBook5", "tempAuthor5", 500, "unread"),
+    new Book("tempBook6", "tempAuthor6", 600, "reading"),
 ];
 
 // Create Book Object
@@ -19,52 +21,86 @@ function Book(title, author, pages, read, info) {
 
 // Create library display
 function displayLibrary(Book) {
+    // Create DOM Elements
     const library = document.getElementById("book-library");
-    const listElement = document.createElement("li");
+    const bookElement = document.createElement("div");
+    const titleP = document.createElement("p");
+    const authorP = document.createElement("p");
+    const pagesP = document.createElement("p");
+    const readP = document.createElement("p");
+    const editStats = document.createElement("button");
     const deleteBook = document.createElement("button");
+
+    // Set Book Title
+    titleP.innerHTML = Book.title;
+    bookElement.appendChild(titleP);
+
+    // Set Book Author
+    authorP.innerHTML = Book.author;
+    bookElement.appendChild(authorP);
+
+    // Set Book Pages
+    pagesP.innerHTML = Book.pages;
+    bookElement.appendChild(pagesP);
+
+    // Set Book Read Status
+    readP.innerHTML = Book.read[0].toUpperCase() + Book.read.slice(1);
+    bookElement.appendChild(readP);
+
+    // Create Edit Status Button **
+
+    // Create Delete Book Button
     deleteBook.setAttribute("class", "delete-book");
     deleteBook.innerHTML = "Delete";
-    listElement.setAttribute("data-index", myLibrary.indexOf(Book));
-    listElement.setAttribute("class", "book");
-    listElement.innerHTML = Book.info();
-    listElement.appendChild(deleteBook);
-    library.appendChild(listElement);
+    bookElement.appendChild(deleteBook);
+
+    // Create Book Element
+    bookElement.setAttribute("data-index", myLibrary.indexOf(Book));
+    bookElement.setAttribute("class", `book ${Book.read}`);
+    library.appendChild(bookElement);
 
     // Create removal feature for books
     const deleteBookButton = document.querySelectorAll(".delete-book");
     deleteBookButton.forEach((button) => {
     button.addEventListener("click", (event) => {
         button.parentElement.remove();
-    })
-});
+        })
+    });
 }
 
-function clearForm(...args) {
-    for (let arg of args) {
-        arg.value = "";
-    }
-}
 
 for (var item of myLibrary) {
     displayLibrary(item);
 }
 
+
+
 // Add new book to library
 const newBookButton = document.getElementById("new-book");
-newBookButton.addEventListener('click', (event) => {
+const dialog = document.querySelector("dialog");
+const submitNewBook = document.querySelector("dialog button");
+
+newBookButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+submitNewBook.addEventListener('click', (event) => {
     let bookTitle = document.getElementById("book-title");
     let bookAuthor = document.getElementById("book-author");
     let bookPages = document.getElementById("book-pages");
-    let bookRead = document.getElementById("book-read");
-    if (!bookRead.checked) {
-        bookRead.value = "not read";
+    let bookProgress = document.getElementsByName("book-progress");
+    let form = document.querySelector("form");
+
+    for (let i = 0; i < bookProgress.length; i++) {
+        if (bookProgress[i].checked) {
+            bookProgress.value = bookProgress[i].value;
+        }
     }
-    else bookRead.value = "read";
-    let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
+
+    let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookProgress.value);
     myLibrary.push(newBook);
     displayLibrary(newBook);
-    clearForm(bookTitle, bookAuthor, bookPages)
-    bookRead.checked = false;
+    dialog.close();
+    form.reset();
     event.preventDefault();
-    console.log(deleteBookButton);
 })
